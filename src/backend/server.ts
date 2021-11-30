59805cac9da9fbf8c69e2e8e35e9b5780aea24bc
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-30 17:44:21 +0000

    Added POST method for adding

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -12,28 +12,25 @@ client.connect().then(() => {
   app.use(express.json());
   app.use(cors());
 
+  // GET ALL TODOS
   app.get("/todos", async (req, res) => {
-    const getAllTodos = await client.query("SELECT * FROM todo;");
+    const result = await client.query("SELECT * FROM todo;");
     res.status(200).json({
       status: "success",
-      data: {
-        getAllTodos,
-      },
+      result,
     });
   });
 
+  // GET A TODO BY ID
   app.get("/todos/:todo_id", async (req, res) => {
     const todo_id = parseInt(req.params.todo_id);
-    const getTodoById = await client.query(
-      "SELECT * FROM todo WHERE id = $1;",
-      [todo_id]
-    );
-    if (getTodoById.rowCount !== 0) {
+    const result = await client.query("SELECT * FROM todo WHERE id = $1;", [
+      todo_id,
+    ]);
+    if (result.rowCount !== 0) {
       res.status(200).json({
         status: "success",
-        data: {
-          getTodoById,
-        },
+        result,
       });
     } else {
       res.status(404).json({
@@ -45,6 +42,43 @@ client.connect().then(() => {
     }
   });
 
+  // UPDATE A TODO BY ID
+  app.put("/todos/:todo_id", async (req, res) => {
+    const todo_id = parseInt(req.params.todo_id);
+    const { description, dueDate } = req.body;
+    const result = await client.query(
+      "UPDATE todo SET description = $1, duedate = $2 WHERE id = $3 RETURNING *;",
+      [description, dueDate, todo_id]
+    );
+    if (result.rowCount !== 0) {
+      res.status(200).json({
+        status: "success",
+        result,
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          id: "Could not find a todo with that id identifier",
+        },
+      });
+    }
+  });
+
+  // ADD A TODO
+  app.post("/todos", async (req, res) => {
+    const { description, dueDate } = req.body;
+    const result = await client.query(
+      "INSERT INTO todo (description, duedate) VALUES ($1, $2) RETURNING *",
+      [description, dueDate]
+    );
+    res.status(201).json({
+      status: "success",
+      result,
+    });
+  });
+
+  // DELETE A TODO BY ID
   app.delete("/todos/:todo_id", async (req, res) => {
     const todo_id = parseInt(req.params.todo_id);
     const deletedTodo = await client.query(
