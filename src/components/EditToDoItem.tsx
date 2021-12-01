2f124f8bb8f5996efca890001622369bab259eb0
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 16:05:32 +0000

    Added unique ID and save button

--- src/components/EditToDoItem.tsx
+++ src/components/EditToDoItem.tsx
@@ -1,6 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { ITodo } from "../ITodo";
+import axios from "axios";
 
-export function EditToDoItem(): JSX.Element {
+export function EditToDoItem(props: { todoItem: ITodo }): JSX.Element {
+  const [newDescription, setNewDescription] = useState<string>(
+    props.todoItem.description
+  );
+  const [newDueDate, setNewDueDate] = useState<string>(props.todoItem.due_date);
+
+  const handleEditItem = (id: number, description: string, dueDate: string) => {
+    axios.put(`https://nchumphot-todo-app.herokuapp.com/todos/${id}`, {
+      description: description,
+      due_date: dueDate,
+      is_complete: props.todoItem.is_complete,
+    });
+  };
+
   return (
     <Fragment>
       {/* <!-- Button to Open the Modal --> */}
@@ -8,25 +23,36 @@ export function EditToDoItem(): JSX.Element {
         type="button"
         className="btn btn-primary"
         data-toggle="modal"
-        data-target="#myModal"
+        data-target={`#todo-${props.todoItem.id}`}
       >
         Edit
       </button>
 
       {/* <!-- The Modal --> */}
-      <div className="modal" id="myModal">
+      <div className="modal" id={`todo-${props.todoItem.id}`}>
         <div className="modal-dialog">
           <div className="modal-content">
             {/* <!-- Modal Header --> */}
             <div className="modal-header">
-              <h4 className="modal-title">Modal Heading</h4>
+              <h4 className="modal-title">Edit to-do</h4>
               <button type="button" className="close" data-dismiss="modal">
                 &times;
               </button>
             </div>
 
             {/* <!-- Modal body --> */}
-            <div className="modal-body">Modal body..</div>
+            <div className="modal-body">
+              <input
+                type="text"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+              <input
+                type="date"
+                value={newDueDate}
+                onChange={(e) => setNewDueDate(e.target.value)}
+              />
+            </div>
 
             {/* <!-- Modal footer --> */}
             <div className="modal-footer">
@@ -34,8 +60,11 @@ export function EditToDoItem(): JSX.Element {
                 type="button"
                 className="btn btn-danger"
                 data-dismiss="modal"
+                onClick={() =>
+                  handleEditItem(props.todoItem.id, newDescription, newDueDate)
+                }
               >
-                Close
+                Save
               </button>
             </div>
           </div>
