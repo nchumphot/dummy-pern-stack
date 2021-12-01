29dfbcc691c2c6bbf09877a8397f8a957f31dc9c
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 12:03:07 +0000

    Added global allTodos and useEffect to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { ToDoList } from "./components/ToDoList";
 import { PageHeader } from "./components/PageHeader";
 import { AddToDoItem } from "./components/AddToDoItem";
+import { useState, useEffect } from "react";
 
 function App(): JSX.Element {
+  const [allTodos, setAllTodos] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://nchumphot-todo-app.herokuapp.com/todos"
+    );
+    const jsonBody = await response.json();
+    // console.log(jsonBody.result.rows);
+    setAllTodos(jsonBody.result.rows);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <PageHeader />
       <AddToDoItem />
-      <ToDoList />
+      <ToDoList todos={allTodos} />
     </>
   );
 }
