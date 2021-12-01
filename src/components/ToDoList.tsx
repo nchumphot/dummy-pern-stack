1eccf2befc10c3c4726d72b227566c0f620765de
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 14:57:35 +0000

    Added handleDeleteItem and formatted table

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,70 @@
 import { ITodo } from "../ITodo";
+import axios from "axios";
+import { EditTodo } from "./EditTodo";
 
 export function ToDoList(props: { todos: ITodo[] }): JSX.Element {
+  const handleDeleteItem = (id: number) => {
+    axios.delete(`https://nchumphot-todo-app.herokuapp.com/todos/${id}`);
+  };
+
+  const handleMarkAsCompleted = (
+    id: number,
+    description: string,
+    dueDate: string,
+    isCompleted: boolean
+  ) => {
+    axios.put(`https://nchumphot-todo-app.herokuapp.com/todos/${id}`, {
+      description: description,
+      due_date: dueDate,
+      is_complete: isCompleted === true ? false : true,
+    });
+    console.log(isCompleted === true ? false : true);
+  };
+
   const todoItem = (item: ITodo): JSX.Element => {
     return (
-      <>
-        <h3>{item.description}</h3>
-        <p>Due date: {item.due_date.substr(0, 10)}</p>
-      </>
+      <tr key={item.id}>
+        <td>
+          <input
+            type="checkbox"
+            defaultChecked={item.is_complete}
+            onClick={() =>
+              handleMarkAsCompleted(
+                item.id,
+                item.description,
+                item.due_date,
+                item.is_complete
+              )
+            }
+          />
+        </td>
+        <td>{item.description}</td>
+        <td>{item.due_date === null ? "None" : item.due_date.substr(0, 10)}</td>
+        <td>
+          <EditTodo />
+        </td>
+        <td>
+          <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+        </td>
+      </tr>
     );
   };
 
   return (
     <>
       <h2>My to-do list:</h2>
-      {props.todos.map((item) => todoItem(item))}
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Checkbox</th>
+            <th scope="col">Description</th>
+            <th scope="col">Due date</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>{props.todos.map((item) => todoItem(item))}</tbody>
+      </table>
     </>
   );
 }
