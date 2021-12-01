3b3a3d9294de3eec156a551460aece81b2b9d3d5
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 15:34:25 +0000

    Initial commit

---
+++ src/components/EditToDoItem.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from "react";
+
+export function EditToDoItem(): JSX.Element {
+  return (
+    <Fragment>
+      {/* <!-- Button to Open the Modal --> */}
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#myModal"
+      >
+        Edit
+      </button>
+
+      {/* <!-- The Modal --> */}
+      <div className="modal" id="myModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            {/* <!-- Modal Header --> */}
+            <div className="modal-header">
+              <h4 className="modal-title">Modal Heading</h4>
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+            </div>
+
+            {/* <!-- Modal body --> */}
+            <div className="modal-body">Modal body..</div>
+
+            {/* <!-- Modal footer --> */}
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
