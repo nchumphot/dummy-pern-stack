8788c165281e084018289b1fb00b2a059066157e
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 12:02:43 +0000

    Display all to-do items

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,3 +1,19 @@
-export function ToDoList(): JSX.Element {
-  return <h1>Main Content</h1>;
+import { ITodo } from "../ITodo";
+
+export function ToDoList(props: { todos: ITodo[] }): JSX.Element {
+  const todoItem = (item: ITodo): JSX.Element => {
+    return (
+      <>
+        <h3>{item.description}</h3>
+        <p>Due date: {item.due_date.substr(0, 10)}</p>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <h2>My to-do list:</h2>
+      {props.todos.map((item) => todoItem(item))}
+    </>
+  );
 }
