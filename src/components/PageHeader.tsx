ffd80c5f83dcb38df5d5fbf0a713e13159240570
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-01 17:11:51 +0000

    Added filter buttons

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,34 @@
-export function PageHeader(): JSX.Element {
-  return <h1>Welcome to my To-do App!</h1>;
+export function PageHeader(props: {
+  filter: string;
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+}): JSX.Element {
+  return (
+    <>
+      <h1>Welcome to my To-do App!</h1>
+      <input
+        type="radio"
+        name="filter"
+        value={0}
+        checked={props.filter === "All"}
+        onClick={() => props.setFilter("All")}
+      />
+      <label htmlFor="All">All</label>{" "}
+      <input
+        type="radio"
+        name="filter"
+        value={1}
+        checked={props.filter === "Uncompleted"}
+        onClick={() => props.setFilter("Uncompleted")}
+      />
+      <label htmlFor="Uncompleted">Uncompleted</label>{" "}
+      <input
+        type="radio"
+        name="filter"
+        value={2}
+        checked={props.filter === "Overdue"}
+        onClick={() => props.setFilter("Overdue")}
+      />
+      <label htmlFor="Overdue">Overdue</label>
+    </>
+  );
 }
