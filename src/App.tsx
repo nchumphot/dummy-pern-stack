c189098732579852ef40ea7621e039a269a69031
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-02 11:13:01 +0000

    Made fetchData a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,22 @@ import { PageHeader } from "./components/PageHeader";
 import { AddToDoItem } from "./components/AddToDoItem";
 import { useState, useEffect } from "react";
 import { ITodo } from "./ITodo";
+import { fetchData } from "./fetchData";
 
 function App(): JSX.Element {
   const [allTodos, setAllTodos] = useState<ITodo[]>([]);
   const filterOptions = ["All", "Uncompleted", "Overdue"];
   const [filter, setFilter] = useState<string>(filterOptions[1]);
 
-  const fetchData = async () => {
-    const response = await fetch(
-      "https://nchumphot-todo-app.herokuapp.com/todos"
-    );
-    const jsonBody = await response.json();
-    // console.log(jsonBody.result.rows);
-    setAllTodos(jsonBody.result.rows);
-  };
-
   useEffect(() => {
-    fetchData();
+    fetchData(setAllTodos);
   }, []);
 
   return (
     <>
       <PageHeader {...{ filter, setFilter }} />
       <AddToDoItem />
-      <ToDoList todos={allTodos} />
+      <ToDoList {...{ allTodos, setAllTodos }} filter={filter} />
     </>
   );
 }
